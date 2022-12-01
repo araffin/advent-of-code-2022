5ccce8def24c4cce3cdc7f8eefe42b5c6724ea2b
Antonin Raffin <antonin.raffin@ensta.org>
2022-12-01 21:41:39 +0100

    Day 1 part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,37 +1,43 @@
-use std::str::FromStr;
+// use std::str::FromStr;
 
-pub fn parse_lines<T: FromStr>(input: &str) -> Result<Vec<T>, String> {
-    input
-        .lines()
-        .enumerate()
-        .map(|(line_idx, line)| {
-            line.parse::<T>()
-                .map_err(|_| format!("Line {}: Not a valid integer", line_idx + 1))
-        })
-        .collect()
-}
+// pub fn parse_lines<T: FromStr>(input: &str) -> Result<Vec<T>, String> {
+//     input
+//         .lines()
+//         .enumerate()
+//         .map(|(line_idx, line)| {
+//             line.parse::<T>()
+//                 .map_err(|_| format!("Line {}: Not a valid integer", line_idx + 1))
+//         })
+//         .collect()
+// }
 
-fn part1(input: &str) {
+fn part1() {
     // Parse the input
-    let parsed_input = parse_lines::<u32>(&input).unwrap();
+    let part1_input = std::fs::read_to_string("input_part1.txt").unwrap();
 
-    let n_lines = parsed_input.len();
+    let mut max_calories = 0;
+    let mut current_calories = 0;
 
-    println!("{} lines", n_lines);
-
-    // Iterate over the lines
-    let mut total_increase = 0;
-    for line in 1..n_lines {
-        if parsed_input[line] >= parsed_input[line - 1] {
-            total_increase += 1;
-        }
+    for line in part1_input.lines() {
+        // Try to parse an int from the line
+        // and catch the error otherwise
+        match line.parse::<i32>() {
+            Ok(n) => {
+                current_calories += n;
+            }
+            Err(_) => {
+                if current_calories >= max_calories {
+                    max_calories = current_calories;
+                }
+                current_calories = 0;
+                continue;
+            }
+        };
     }
-    // Print the total_increase
-    println!("{}", total_increase);
+    // Print the max_calories
+    println!("max_calories={}", max_calories);
 }
 
 fn main() {
-    let part1_input = include_str!("../input.txt");
-    // let part1_input = std::fs::read_to_string("input.txt").unwrap();
-    part1(&part1_input);
+    part1();
 }
