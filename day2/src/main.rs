47057f85ca2f89e6fc08410cf4cc67f1c5425e1b
Antonin Raffin <antonin.raffin@ensta.org>
2022-12-02 10:33:53 +0100

    Day2 part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -29,7 +29,54 @@ fn part1() {
     println!("Total score: {}", total_score);
 }
 
-fn part2() {}
+fn part2() {
+    // Parse the input
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let mut total_score = 0;
+
+    for line in input.lines() {
+        let (player1, player2) = line.split_once(" ").unwrap();
+
+        let score = match (convert_to_score(player1), player2) {
+            // We need to lose
+            (player1, "X") => {
+                match player1 {
+                    // rock against paper
+                    2 => 1,
+                    // paper against scissors
+                    3 => 2,
+                    // scissors against rock
+                    1 => 3,
+                    _ => panic!("Invalid input"),
+                }
+            }
+            // We need to end with a draw
+            (player1, "Y") => {
+                // We play the same as player 1
+                player1 + 3
+            }
+            // We need to win
+            (player1, "Z") => {
+                let player2 = match player1 {
+                    // paper against rock
+                    1 => 2,
+                    // scissors against paper
+                    2 => 3,
+                    // rock against scissors
+                    3 => 1,
+                    _ => panic!("Invalid input"),
+                };
+                player2 + 6
+            }
+
+            _ => panic!("Invalid input"),
+        };
+
+        total_score += score;
+    }
+    println!("Total score part2: {}", total_score);
+}
 
 fn main() {
     part1();
