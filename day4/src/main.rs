f4d962a6efe78fd9b27bda1864caab5b93351323
Antonin Raffin <antonin.raffin@ensta.org>
2022-12-04 14:09:35 +0100

    Day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,6 +2,12 @@ fn is_contained(low1: u32, high1: u32, low2: u32, high2: u32) -> bool {
     (low1 >= low2 && high1 <= high2) || (low2 >= low1 && high2 <= high1)
 }
 
+fn check_overlap(low1: u32, high1: u32, low2: u32, high2: u32) -> bool {
+    // Check if there the two intervals are disjoint
+    // and negate the result
+    !(high1 < low2 || high2 < low1)
+}
+
 fn part1() {
     // Parse the input
     let input = std::fs::read_to_string("input.txt").unwrap();
@@ -25,7 +31,28 @@ fn part1() {
     println!("{}", total);
 }
 
-fn part2() {}
+fn part2() {
+    // Parse the input
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let mut total: u32 = 0;
+
+    for line in input.lines() {
+        let (first_elf, second_elf) = line.split_once(',').unwrap();
+        let (low1, high1) = first_elf.split_once('-').unwrap();
+        let (low2, high2) = second_elf.split_once('-').unwrap();
+        // Parse the numbers into a int tuple
+        let (low1, high1, low2, high2) = (
+            low1.parse::<u32>().unwrap(),
+            high1.parse::<u32>().unwrap(),
+            low2.parse::<u32>().unwrap(),
+            high2.parse::<u32>().unwrap(),
+        );
+        total += check_overlap(low1, high1, low2, high2) as u32;
+    }
+    // Print the total
+    println!("{}", total);
+}
 
 fn main() {
     part1();
