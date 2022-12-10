d02d6d32d92b7415b382819e6032e95ed4719ad2
Antonin Raffin <antonin.raffin@ensta.org>
2022-12-10 13:38:54 +0100

    Day10 part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,10 +6,10 @@ fn part1() {
     let mut total_signal_strength: i32 = 0;
 
     for line in input.lines() {
-        let (cycles, new_value) = match line {
+        let (cycles, register_offset) = match line {
             "noop" => (1, 0),
             _ => {
-                let value = line.split_once(" ").unwrap().1.parse::<i32>().unwrap();
+                let value = line.split_once(' ').unwrap().1.parse::<i32>().unwrap();
                 (2, value)
             }
         };
@@ -19,12 +19,49 @@ fn part1() {
                 total_signal_strength += register_value * n_cycles;
             }
         }
-        register_value += new_value;
+        register_value += register_offset;
         // println!("{}: {} + {} = {}", n_cycles, register_value - new_value, new_value, register_value);
     }
     println!("Part 1: {}", total_signal_strength);
 }
 
+fn part2() {
+    // Parse the input
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let mut register_value: i32 = 1;
+    let mut n_cycles: i32 = 0;
+    let sprite = "###.....................................";
+    let line_length = 40;
+
+    for line in input.lines() {
+        let (cycles, register_offset) = match line {
+            "noop" => (1, 0),
+            _ => {
+                let value = line.split_once(' ').unwrap().1.parse::<i32>().unwrap();
+                (2, value)
+            }
+        };
+        for _ in 0..cycles {
+            // cursor position
+            let cursor_position = n_cycles % line_length;
+            let offset = register_value - 1;
+            // use modulo to wrap around (-3 % 40 = 37)
+            let sprite_idx = ((cursor_position - offset % line_length) + line_length) % line_length;
+            let current_pixel = sprite.chars().nth(sprite_idx as usize).unwrap();
+
+            print!("{}", current_pixel);
+
+            n_cycles += 1;
+
+            if n_cycles % line_length == 0 {
+                println!()
+            }
+        }
+        register_value += register_offset;
+    }
+}
+
 fn main() {
     part1();
+    part2();
 }
