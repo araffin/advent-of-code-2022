311c1b39ce33b13ad37323d9d1f9739b338d853d
Antonin Raffin <antonin.raffin@ensta.org>
2022-12-11 13:47:55 +0100

    Simplify part2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -123,51 +123,6 @@ fn part1() {
     println!("Part 1: {}", monkey_business);
 }
 
-fn process_one_monkey(
-    i: usize,
-    monkeys: &mut [Monkey],
-    items_remainder: &mut [VecDeque<Vec<usize>>],
-) {
-    let monkey = monkeys[i].clone();
-    let items = items_remainder[i].clone();
-
-    for remainder_per_monkey in items {
-        monkeys.get_mut(i).unwrap().n_inspections += 1;
-
-        let mut new_remainders: Vec<usize> = Vec::new();
-
-        for (idx, remainder) in remainder_per_monkey.iter().enumerate() {
-            let value = match monkey.operations[1].parse::<usize>() {
-                Ok(v) => v,
-                Err(_) => *remainder,
-            };
-
-            let new_value = match monkey.operations[0].as_str() {
-                "+" => remainder + value,
-                "*" => remainder * value,
-                _ => panic!("Unknown operation"),
-            } % monkeys[idx].divisible_by;
-
-            new_remainders.push(new_value);
-        }
-
-        let next_monkey = if new_remainders[i] == 0 {
-            monkey.next_monkeys[0]
-        } else {
-            monkey.next_monkeys[1]
-        };
-
-        // Move the item to the next monkey
-        // we need to remove it from the current monkey first
-
-        items_remainder.get_mut(i).unwrap().pop_front();
-        items_remainder
-            .get_mut(next_monkey)
-            .unwrap()
-            .push_back(new_remainders);
-    }
-}
-
 fn part2() {
     // Parse the input
     let input = std::fs::read_to_string("input.txt").unwrap();
@@ -229,30 +184,51 @@ fn part2() {
             n_inspections: 0,
         });
     }
+    // println!("{:?}", monkeys);
 
-    // we store the remainder of each item for each monkey
-    // to avoid overflow
-    let mut items_remainder: Vec<VecDeque<Vec<usize>>> = Vec::new();
-    for monkey in &monkeys {
-        let mut items: VecDeque<Vec<usize>> = VecDeque::new();
-        for item in &monkey.items {
-            let mut remainder_per_monkey: Vec<usize> = Vec::new();
-            for monkey in &monkeys {
-                remainder_per_monkey.push(item % monkey.divisible_by);
-            }
-            items.push_back(remainder_per_monkey);
-        }
-        items_remainder.push(items);
-    }
+    let modulus = monkeys.iter().map(|m| m.divisible_by).product::<usize>();
 
     for _ in 0..n_rounds {
         for i in 0..monkeys.len() {
-            process_one_monkey(i, &mut monkeys, &mut items_remainder);
+            // Copy the current monkey to be able
+            // to modify the list later
+            let monkey = monkeys[i].clone();
+
+            for item in monkey.items {
+                monkeys.get_mut(i).unwrap().n_inspections += 1;
+
+                let value = match monkey.operations[1].parse::<usize>() {
+                    Ok(v) => v,
+                    Err(_) => item,
+                };
+
+                let mut new_value = match monkey.operations[0].as_str() {
+                    "+" => item + value,
+                    "*" => item * value,
+                    _ => panic!("Unknown operation"),
+                };
+                // Use modulus to avoid overflow
+                new_value %= modulus;
+
+                let next_monkey = if new_value % monkey.divisible_by == 0 {
+                    monkey.next_monkeys[0]
+                } else {
+                    monkey.next_monkeys[1]
+                };
+                // Move the item to the next monkey
+                // we need to remove it from the current monkey first
+                monkeys.get_mut(i).unwrap().items.pop_front();
+                monkeys
+                    .get_mut(next_monkey)
+                    .unwrap()
+                    .items
+                    .push_back(new_value);
+            }
         }
     }
 
     // for (idx, monkey) in monkeys.iter().enumerate() {
-    //     println!("{} {}", idx, monkey.n_inspections);
+    //     println!("{} {:?} {}", idx, monkey.items, monkey.n_inspections);
     // }
 
     let mut n_inspections: Vec<usize> = monkeys.iter().map(|m| m.n_inspections).collect();
